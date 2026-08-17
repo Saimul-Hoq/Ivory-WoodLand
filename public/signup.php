5a22d07050870d6f43f581eb9cdb37aabf501347
Saimul-Hoq <saimulhoque2000@gmail.com>
2026-08-18 04:12:21 +0600

    Completed designing the frondtend of login and signup

---
+++ public/signup.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ivory-WoodLand|login</title>
+    <link rel="shortcut icon" href="./assets/logo-round.png" type="image/x-icon">
+    
+    <!-- font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.0/css/all.min.css" integrity="sha512-ApSLB1Pd3/bZN8fWB/RG9YhN/7bd9Hkf3AGaE2mPfebjrxagjuBtx2GcgdqIlJkUzwylBo61r9Xa9NmgBI0swA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/signup.css">
+</head>
+<body>
+    <div class="body-area">
+        <div class="card">
+            <div class="card-start">
+                <img src="./assets/logo-mobile_wb.png" alt="">
+            </div>
+            <div class="card-end">
+                <h1 class="text-center font">Register your account — <br>your woodland begins here.</h1>
+                <fieldset>
+                    <div class="fieldset-container">
+                    <div class="left">
+                        <label class="label text-md">Name: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="text" class="input" placeholder="Enter your name" />
+                        <label class="label text-md">Email: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="text" class="input" placeholder="Enter your email" />
+                        <label class="label text-md">Password: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="password" class="input" placeholder="Enter your password" />
+
+                        
+                        <a href="./login.php"><button id="login-btn" class="btn btn-neutral">Back</button></a>
+                        
+                        <!-- <p class="text-center">Don't have an account? <a href="./signup.php">signup</a> </p> -->
+                    </div>
+
+                    <div class="right">
+                        <label class="label text-md">Phone Number: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="text" class="input" placeholder="01XXXXXXXXX" />
+                        <label class="label text-md">Avatar: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="text" accept="image/*" class="input" placeholder="Enter your profile photo" />
+                        <label class="label text-md">Address: </label> <br>
+                        <p id="login-email-error"></p>
+                        <input id="login-email" type="text" class="input" placeholder="Enter your address" />
+
+                       
+
+                        <button id="login-btn" class="btn btn-primary">Register</button>
+                        <!-- <p class="text-center">Don't have an account? <a href="./signup.php">signup</a> </p> -->
+                    </div>
+                    </div>
+                    
+               
+                </fieldset>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
