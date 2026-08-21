e024001b78333ad9d47749d6a80b5da7a6dd1237
Saimul-Hoq <saimulhoque2000@gmail.com>
2026-08-22 01:19:01 +0600

    Designed side panel

---
+++ public/js/dashboard.js
@@ -0,0 +1,7 @@
+document.querySelector("#profile-btn").addEventListener("click", () => {
+    document.querySelector("#dropdown-menu").classList.toggle("open-menu");
+})
+
+document.querySelector("#sidepanel-btn").addEventListener("click", () => {
+    document.querySelector(".sidepanel").classList.toggle("open-sidepanel");
+})
